6525a8ad3333ba0b2baeb4241532558b65c4552d
amarten <alexdmarten@gmail.com>
2022-01-03 13:04:24 -0800

    store multiple project answers

--- app.js
+++ app.js
@@ -26,12 +26,83 @@ const inquirer = require('inquirer');
 //     console.log('Portfolio complete! Check out index.html to see the output!');
 // });
 
-inquirer
-    .prompt([
+const promptUser = () => {
+    return inquirer.prompt([
         {
             type: 'input',
             name: 'name',
             message: 'What is your name?'
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: 'What is your GitHub account name?'
+        },
+        {
+            type: 'input',
+            name: 'about',
+            message: 'Provide some information about yourself.'
+        }
+    ]);
+};
+
+const prompProject = portfolioData => {
+    if (!portfolioData.projects) {
+        portfolioData.projects = [];
+    }
+
+    console.log(`
+=================
+Add a New Project
+=================
+    `);
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is the name of your project?'
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Provide a description of the project (Required)'
+        },
+        {
+            type: 'checkbox',
+            name: 'languages',
+            message: 'What did you build this project with? (Check all that apply)',
+            choices: ['JavaScript', 'HTML', 'CSS', 'ES6', 'jQuery', 'Bootstrap', 'Node']
+        },
+        {
+            type: 'input',
+            name: 'link',
+            message: 'Enter the GitHub link to your project. (Required)'
+        },
+        {
+            type: 'confirm',
+            name: 'feature',
+            message: 'Would you like to feature this project?',
+            default: false
+        },
+        {
+            type: 'confirm',
+            name: 'confirmAddProject',
+            message: 'Would you like to enter another project?',
+            default: false
         }
     ])
-    .then(answers => console.log(answers));
+    .then(projectData => {
+        portfolioData.projects.push (projectData);
+        if(projectData.confirmAddProject) {
+            return prompProject(portfolioData);
+        } else {
+            return portfolioData;
+        }
+    });
+};
+
+promptUser()
+    .then(prompProject)
+    .then(portfolioData => {
+        console.log(portfolioData);
+    });
